61c7f36619c71b0e79d89b39defe22b95e1988ff
Jack Christensen <jack@jackchristensen.com>
2013-06-20 11:32:25 -0500

    Reopen existing ActiveRecord classes for migrations
    extend wasn't working properly in the case of
    create_table do |t|
      t.hstore :attributes
    end
    
    Ideally, this should be done with Module#prepend, but that would
    require Ruby 2.0

--- lib/surus/hstore/connection_adapters.rb
+++ lib/surus/hstore/connection_adapters.rb
@@ -1,54 +1,46 @@
-module Surus
-  module Hstore
-    module ConnectionAdapters
+require 'active_record/connection_adapters/postgresql_adapter'
 
-      class PostgreSQLColumn < ActiveRecord::ConnectionAdapters::Column
-        def simplified_type_with_hstore(field_type)
-          field_type == 'hstore' ? :hstore : simplified_type_without_hstore(field_type)
-        end
+ActiveRecord::ConnectionAdapters::PostgreSQLColumn.class_eval do
+  def simplified_type_with_hstore(field_type)
+    field_type == 'hstore' ? :hstore : simplified_type_without_hstore(field_type)
+  end
 
-        alias_method_chain :simplified_type, :hstore
-      end
+  alias_method_chain :simplified_type, :hstore
+end
 
-      class PostgreSQLAdapter < ActiveRecord::ConnectionAdapters::AbstractAdapter
-        def native_database_types_with_hstore
-          native_database_types_without_hstore.merge({:hstore => { :name => "hstore" }})
-        end
 
-        alias_method_chain :native_database_types, :hstore
-      end
+ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
+  def native_database_types_with_hstore
+    native_database_types_without_hstore.merge({:hstore => { :name => "hstore" }})
+  end
 
-      class TableDefinition
-        # Adds hstore type for migrations. So you can add columns to a table like:
-        #   create_table :people do |t|
-        #     ...
-        #     t.hstore :info
-        #     ...
-        #   end
-        def hstore(*args)
-          options = args.extract_options!
-          column_names = args
-          column_names.each { |name| column(name, 'hstore', options) }
-        end
+  alias_method_chain :native_database_types, :hstore
+end
 
-      end
-
-      class Table
-        # Adds hstore type for migrations. So you can add columns to a table like:
-        #   change_table :people do |t|
-        #     ...
-        #     t.hstore :info
-        #     ...
-        #   end
-        def hstore(*args)
-          options = args.extract_options!
-          column_names = args
-          column_names.each { |name| column(name, 'hstore', options) }
-        end
-
-      end
-    end
+ActiveRecord::ConnectionAdapters::TableDefinition.class_eval do
+  # Adds hstore type for migrations. So you can add columns to a table like:
+  #   create_table :people do |t|
+  #     ...
+  #     t.hstore :info
+  #     ...
+  #   end
+  def hstore(*args)
+    options = args.extract_options!
+    column_names = args
+    column_names.each { |name| column(name, 'hstore', options) }
   end
 end
 
-ActiveRecord::ConnectionAdapters.extend Surus::Hstore::ConnectionAdapters
+ActiveRecord::ConnectionAdapters::Table.class_eval do
+  # Adds hstore type for migrations. So you can add columns to a table like:
+  #   change_table :people do |t|
+  #     ...
+  #     t.hstore :info
+  #     ...
+  #   end
+  def hstore(*args)
+    options = args.extract_options!
+    column_names = args
+    column_names.each { |name| column(name, 'hstore', options) }
+  end
+end
--- lib/surus/json/connection_adapters.rb
+++ lib/surus/json/connection_adapters.rb
@@ -1,54 +1,46 @@
-module Surus
-  module JSON
-    module ConnectionAdapters
+require 'active_record/connection_adapters/postgresql_adapter'
 
-      class PostgreSQLColumn < ActiveRecord::ConnectionAdapters::Column
-        def simplified_type_with_json(field_type)
-          field_type == 'json' ? :json : simplified_type_without_json(field_type)
-        end
+ActiveRecord::ConnectionAdapters::PostgreSQLColumn.class_eval do
+  def simplified_type_with_json(field_type)
+    field_type == 'json' ? :json : simplified_type_without_json(field_type)
+  end
 
-        alias_method_chain :simplified_type, :json
-      end
+  alias_method_chain :simplified_type, :json
+end
 
-      class PostgreSQLAdapter < ActiveRecord::ConnectionAdapters::AbstractAdapter
-        def native_database_types_with_json
-          native_database_types_without_json.merge({:json => { :name => "json" }})
-        end
 
-        alias_method_chain :native_database_types, :json
-      end
+ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
+  def native_database_types_with_json
+    native_database_types_without_json.merge({:json => { :name => "json" }})
+  end
 
-      class TableDefinition
-        # Adds json type for migrations. So you can add columns to a table like:
-        #   create_table :people do |t|
-        #     ...
-        #     t.json :info
-        #     ...
-        #   end
-        def json(*args)
-          options = args.extract_options!
-          column_names = args
-          column_names.each { |name| column(name, 'json', options) }
-        end
+  alias_method_chain :native_database_types, :json
+end
 
-      end
-
-      class Table
-        # Adds json type for migrations. So you can add columns to a table like:
-        #   change_table :people do |t|
-        #     ...
-        #     t.json :info
-        #     ...
-        #   end
-        def json(*args)
-          options = args.extract_options!
-          column_names = args
-          column_names.each { |name| column(name, 'json', options) }
-        end
-
-      end
-    end
+ActiveRecord::ConnectionAdapters::TableDefinition.class_eval do
+  # Adds json type for migrations. So you can add columns to a table like:
+  #   create_table :people do |t|
+  #     ...
+  #     t.json :info
+  #     ...
+  #   end
+  def json(*args)
+    options = args.extract_options!
+    column_names = args
+    column_names.each { |name| column(name, 'json', options) }
   end
 end
 
-ActiveRecord::ConnectionAdapters.extend Surus::JSON::ConnectionAdapters
+ActiveRecord::ConnectionAdapters::Table.class_eval do
+  # Adds json type for migrations. So you can add columns to a table like:
+  #   change_table :people do |t|
+  #     ...
+  #     t.json :info
+  #     ...
+  #   end
+  def json(*args)
+    options = args.extract_options!
+    column_names = args
+    column_names.each { |name| column(name, 'json', options) }
+  end
+end
