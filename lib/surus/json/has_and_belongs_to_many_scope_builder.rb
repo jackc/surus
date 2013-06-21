43767448aa906ca6efb48c56f8c2fd759142d354
Jack Christensen <jack@jackchristensen.com>
2013-06-21 14:57:34 -0500

    Fix JSON for Rails 4

--- lib/surus/json/has_and_belongs_to_many_scope_builder.rb
+++ lib/surus/json/has_and_belongs_to_many_scope_builder.rb
@@ -2,17 +2,16 @@ module Surus
   module JSON
     class HasAndBelongsToManyScopeBuilder < AssociationScopeBuilder
       def scope
-        association_scope = association
+        s = association
           .klass
           .joins("JOIN #{join_table} ON #{join_table}.#{association_foreign_key}=#{association_table}.#{association_primary_key}")
           .where("#{outside_class.quoted_table_name}.#{association_primary_key}=#{join_table}.#{foreign_key}")
-        association_scope = association_scope.where(conditions) if conditions
-        association_scope = association_scope.order(order) if order
-        association_scope
+        s = s.instance_eval(&association.scope) if association.scope
+        s
       end
 
       def join_table
-        connection.quote_table_name association.options[:join_table]
+        connection.quote_table_name association.join_table
       end
 
       def association_foreign_key
